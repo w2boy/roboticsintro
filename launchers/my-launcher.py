0c03dc1042f1da0c0914e24ed86dc4ca555a9a3e
w2boy <wesleyt.wardle@gmail.com>
2026-09-15 19:01:39 -0600

    Lab1 Finished part 4 tutorials

---
+++ launchers/my-launcher.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+message = "This is a Python launcher"
+print(message)
---
+++ packages/my_package/my_script.py
@@ -0,0 +1,5 @@
+import os
+
+vehicle_name = os.environ['VEHICLE_NAME']
+message = f"\nHello from {vehicle_name}!\n"
+print(message)
